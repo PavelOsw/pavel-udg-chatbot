be890982379589d1b8ad59636270f1ef0e07a4a7
PavelOsw <pavel.osw117@gmail.com>
2023-10-14 14:11:09 -0600

    delete component, change project path

--- src/app/chatbot/components/message-card/message-card.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, inject } from '@angular/core';
-
-@Component({
-  selector: 'chatbot-message-card',
-  templateUrl: './message-card.component.html',
-  styleUrls: ['./message-card.component.scss']
-})
-export class MessageCardComponent {
-}
