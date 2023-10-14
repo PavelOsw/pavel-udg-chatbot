0f92de9641f25e7435920e96fa7a3155a8e7ec48
PavelOsw <pavel.osw117@gmail.com>
2023-10-14 12:17:54 -0600

    update main app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+  },
+  {
+    path: 'chatbot',
+    loadChildren: () => import('./chatbot/chatbot.module').then( m => m.ChatBotModule ),
+  },
+  {
+    path: '404',
+    component: Error404PageComponent
+  },
+  {
+    path: '**',
+    redirectTo: '404',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
