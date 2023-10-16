51cb582ea347394b3e689cf828c95e7da1da58db
PavelOsw <pavel.osw117@gmail.com>
2023-10-15 23:00:33 -0600

    update styles in line modified

--- src/app/chatbot/pages/post-page/post-page.component.ts
+++ src/app/chatbot/pages/post-page/post-page.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
-  styles: []
+  styles: [`
+    .form-container mat-form-field + mat-form-field {
+      margin-left: 10px;
+    }
+  `]
 })
 export class PostPageComponent {
 
